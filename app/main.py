f5c480d71ed6239cd12760f645d78bc409e5039c
Jichul54 <kjc778869@gmail.com>
2023-11-03 23:19:45 +0900

    Enable /get_pose_image/ endpoint to successfully run OpenPose and return results

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+import cv2
+import numpy as np
+
+import sys
+sys.path.append("..")
+from run import get_pose_image_from_file
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/get_pose_image/")
+async def get_pose_image():
+    result_img = get_pose_image_from_file(
+        image_path='/Users/kimjichul/Desktop/dev/bodhi/bodhi-backend/images/p1.jpg'
+    )  # 원하는 이미지 경로 설정
+    _, encoded_image = cv2.imencode('.png', result_img)
+    return Response(content=encoded_image.tobytes(), media_type="image/png")
